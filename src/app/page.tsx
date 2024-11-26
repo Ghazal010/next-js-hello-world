fc0a9cf829dc899fbab3194a7d436bf90a177cd7
Ghazal <user@Ghazal.local>
2024-11-26 17:26:45 +0500

    Fixed unescaped characters issue 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import next from "next"
 export default function Hello(){
   return(
     <div className="bg-yellow-300 h-screen w-screen">
     <div className="bg-blue-950 flex justify-center mt-4">
       <h1 className="text-white font-semibold text-6xl items-center font-serif">Hello World</h1>
     </div>
-    <p className="text-center text-4xl text-amber-950 font-serif m-6">This is my first Next.js page! <br/>I am taking my first steps into the world of Next.js, and it is really exciting to explore this new framework. <br/>Using Next.js gives me the opportunity to take my web development skills to the next level. <br/>With this page, I am learning the basic concepts of Next.js & planning to implement them in my future projects. <br/>This is just the beginning, & I am hopeful that Next.js will be a useful and fun tool for me!</p>
+    <p className="text-center text-4xl text-amber-950 font-serif m-6">This is my first Next.js page! <br/>I am taking my first steps into the world of Next.js, and it is really exciting to explore this new framework. <br/>Using Next.js gives me the opportunity to take my web development skills to the next level. <br/>With this page, I am learning the basic concepts of Next.js and planning to implement them in my future projects. <br/>This is just the beginning, and I am hopeful that Next.js will be a useful and fun tool for me!</p>
     </div>
   )
 }
